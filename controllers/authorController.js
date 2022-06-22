00923daee8f584f8bc6767ec42450ef64a5b2e05
neil dela cruz <genoznightmare@gmail.com>
2022-06-22 22:43:12 +0800

    author_list page

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,8 +1,19 @@
 var Author = require("../models/author");
-
+var navlinks = require("./modules/navlinks");
 // Display list of all Authors.
-exports.author_list = function (req, res) {
-  res.send("NOT IMPLEMENTED: Author list");
+exports.author_list = function (req, res, next) {
+  Author.find()
+    .sort({ family_name: "ascending" })
+    .exec((err, list_authors) => {
+      if (err) {
+        return next(err);
+      }
+      res.render("author_list", {
+        title: "Author List",
+        author_list: list_authors,
+        navlinks,
+      });
+    });
 };
 
 // Display detail page for a specific Author.
